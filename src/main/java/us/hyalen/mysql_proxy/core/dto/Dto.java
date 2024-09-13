2f3e52fa488d6af54b379cae7044aab266c1923d
Hyalen M. Caldeira <hyalen@icloud.com>
2024-09-12 20:27:54 -0400

    Java downgrade done and works fine. This is the functinoal version.

--- src/main/java/us/hyalen/mysql_proxy/core/dto/Dto.java
+++ src/main/java/us/hyalen/mysql_proxy/core/dto/Dto.java
@@ -1,12 +1,16 @@
 package us.hyalen.mysql_proxy.core.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import us.hyalen.mysql_proxy.model.BaseModel;
+
+import java.io.Serializable;
 
 /**
  * Base Data Transfer Object
  * <p>Note: Rule of thumb: Every model <b>MUST</b> inherit from the Dto. It will give us a common base that will
  * help in future in injecting common functionality as well as in building non-intrusive logics such as aspects</p>
  */
-public abstract class Dto extends BaseModel {
+@JsonSerialize
+public abstract class Dto extends BaseModel implements Serializable {
 
 }
--- src/main/java/us/hyalen/mysql_proxy/core/dto/ErrorDto.java
+++ src/main/java/us/hyalen/mysql_proxy/core/dto/ErrorDto.java
@@ -2,18 +2,15 @@ package us.hyalen.mysql_proxy.core.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.Serializable;
 
 /**
  * This is a solitary model designed for capturing error details supporting ResponseDto
  */
-@Data
-@AllArgsConstructor
+@JsonSerialize
 public class ErrorDto extends Dto implements Serializable {
     private static final long serialVersionUID = 1L;
     private static final Logger LOGGER = LoggerFactory.getLogger(ErrorDto.class);
@@ -35,5 +32,37 @@ public class ErrorDto extends Dto implements Serializable {
     public ErrorDto(String code, String message) {
         this.code = code;
         this.message = message;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getInternalMessage() {
+        return internalMessage;
+    }
+
+    public void setInternalMessage(String internalMessage) {
+        this.internalMessage = internalMessage;
     }
 }
